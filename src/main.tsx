158c12fd4cf09d0d999b1b206c6a7c3055607035
woodpekcerAnos <woodpeckerAnos@foxmail.com>
2021-05-06 21:30:49 +0800

    初始化样式

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 /*
  * @Author: your name
  * @Date: 2021-05-06 20:36:40
- * @LastEditTime: 2021-05-06 21:19:19
+ * @LastEditTime: 2021-05-06 21:29:41
  * @LastEditors: Please set LastEditors
  * @Description: 入口TS文件
  */
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from '@/App'
+import '@/assets/styles/index.scss'
 
 ReactDOM.render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -4,31 +4,25 @@ const path = require('path')
 const { resolve } = path
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [reactRefresh()],
-  //打包路径
-  base:  './', 
-  resolve: {
-    // 别名
-    alias: { 
-      '@': path.resolve(__dirname, 'src'),
-      'utils': path.resolve(__dirname, 'src', 'utils')
-    }
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        login: resolve(__dirname, 'login', 'index.html')
+export default {
+  ...defineConfig({
+    plugins: [reactRefresh()],
+    //打包路径
+    base:  './', 
+    resolve: {
+      // 别名
+      alias: { 
+        '@': path.resolve(__dirname, 'src'),
+        'utils': path.resolve(__dirname, 'src', 'utils')
+      }
+    },
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          login: resolve(__dirname, 'login', 'index.html')
+        }
       }
     }
-  },
-  css: {
-    preprocessorOptions: { 
-      scss: {
-      // 全局的scss ，跨域放多个，例如：主题的变量，和一些混合等
-        additionalData: '@import "./src/assets/styles/index.scss";',
-      }
-    }
-  },
-})
+  })
+}
