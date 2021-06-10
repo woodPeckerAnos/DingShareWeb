878941b8310588e770d3ad4fea1b2620fb0c5215
woodpeckerAnos <woodpeckeranos@foxmail.com>
2021-06-10 15:54:46 +0800

    页面开发推进

--- src/pages/homePage/components/MiddleBanner.tsx
+++ src/pages/homePage/components/MiddleBanner.tsx
@@ -1,15 +1,16 @@
 /*
  * @Author: WoodpeckerAnos
  * @Date: 2021-05-21 21:09:13
- * @LastEditTime: 2021-06-05 22:07:58
+ * @LastEditTime: 2021-06-10 15:08:59
  * @LastEditors: WoodpeckerAnos
  * @Description: 中等大小的轮播展示 -> 热门游戏展示
  */
-import React from 'react'
+import React, { useState } from 'react'
 import classNames from 'classnames'
 import { Carousel } from 'UI/main'
 
 function MiddleBanner() {
+    const [bannerGamePages, changeBanner] = useState<any[]>([{},{},{},{}])
     return (
         <div
             className={classNames('dingshare-normal-background', 'dingshare-middle-banner-hot')}
@@ -17,7 +18,30 @@ function MiddleBanner() {
             <div
                 className={classNames('middle-banner-center-container')}
             >
-
+                <Carousel effect='fade'>
+                    {
+                        bannerGamePages.map((banner, index) => (
+                            <div 
+                                className={classNames(
+                                    'carousel-page-container', 
+                                    `carousel-page-${index}`
+                                )}
+                            >
+                                <h3 
+                                    style={{
+                                        height: '100%',
+                                        color: '#fff',
+                                        lineHeight: '160px',
+                                        textAlign: 'center',
+                                        background: '#364d79',
+                                    }}
+                                >
+                                    { index }
+                                </h3>
+                            </div>
+                        ))
+                    }
+                </Carousel>
             </div>
         </div>
     )
